55abec81f3a85d0ba1b422f0b238363b26c92244
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-24 14:05:45 +1000

    feat: Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# H.Controls
+
+## WPF
+
+### NavigationView
+![NavigationView](docs/avigationView.png)
+
+Usage
+```xml
+```
---
+++ src/apps/H.Controls.Apps.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace UpworkPdfGenerator.Apps;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
